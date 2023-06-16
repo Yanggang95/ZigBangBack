6097763fad3a904968cecdee30901f356b3d308f
aypd02150@gmail.com <aypd02150@gmail.com>
2023-06-17 00:05:46 +0900

    second

---
+++ src/main/java/com/example/demo/Hello.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @GetMapping("/")
+    public String hello(
+            @RequestParam(value = "name", defaultValue = "World") String name,
+            @RequestParam(value = "hi", defaultValue = "Hello") String hi
+    ) {
+        return hi +" "+ name;
+
+    }
+}
